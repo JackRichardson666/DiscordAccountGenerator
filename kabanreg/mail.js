b0791b0a6fa4d84b99c5f306a0c986fdaa289abb
JackRichardson666 <64367044+JackRichardson666@users.noreply.github.com>
2022-02-03 17:41:29 +0300

    основные файлы без вебдрайвера и tor
    доп файлы (вебдрайвер + tor) - https://cloud.mail.ru/public/kvmu/nAbzp7BTB

---
+++ kabanreg/mail.js
@@ -0,0 +1,67 @@
+let imaps = require('imap-simple');
+const simpleParser = require('mailparser').simpleParser;
+const _ = require('lodash');
+const fs = require(`fs`)
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+module.exports = {
+    getVerLink: function(str,csrf) {
+        return new Promise((resolve, reject) => {
+            mail.getMessage(str, csrf, function(e, b) {
+                if (e) return resolve({success: false,error:e}) ;
+                resolve({
+                    success: true,
+                    message: `https://click.discord.com/` + b.split(`https://click.discord.com/`)[2].split("\"")[0]
+                });
+            })
+        })
+    },
+
+    pickmail: function() {
+        const read = JSON.parse(fs.readFileSync(`./mails.json`,`utf-8`));
+        const name = Object.keys(read)[Math.floor(Math.random() * Object.keys(read).length)];
+        const pass = read[name];
+        return {name: name,pass: pass};
+    },
+
+    getmessage: function(mail,password) {
+
+        return new Promise((resolve, reject) => {
+            const config = {
+                imap: {
+                    user: mail+"@gmail.com",
+                    password: password,
+                    host: 'imap.gmail.com',
+                    port: 993,
+                    tls: true,
+                    authTimeout: 3000
+                }
+            };
+
+            imaps.connect(config).then(function (connection) {
+                return connection.openBox('INBOX').then(function () {
+                    const searchCriteria = ['UNSEEN'];
+                    const fetchOptions = {
+                        bodies: ['HEADER', 'TEXT', ''],
+                        markSeen: true
+                        };;
+                    return connection.search(searchCriteria, fetchOptions).then(function (messages) {
+                        messages.forEach(function (item) {
+                            const all = _.find(item.parts, { "which": "" })
+                            const id = item.attributes.uid;
+                            const idHeader = "Imap-Id: "+id+"\r\n";
+                            simpleParser(idHeader+all.body, (err, mail) => {
+                                if(mail.subject.search(`Discord`) > -1){
+                                    const krut = mail.html.split(`Подтвердить e-mail`)[0].slice(-2000).match(/http[^ ]+/g)[0].slice(0,-1);
+                                    resolve(krut)
+                                }
+                            });
+                        });
+                    });
+                });
+            });
+
+        })
+    }
+}
---
+++ kabanreg/main.js
@@ -0,0 +1,207 @@
+// Modules
+const mailwork = require("./mail");
+const puppeteer = require('puppeteer-extra'); // сам пупитир
+const UsernameGenerator = require('username-generator'); // генератор имен
+const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha'); // решалка капчи
+const StealthPlugin = require('puppeteer-extra-plugin-stealth') //мега стелс by мегамозг slappy
+var userAgent = require('random-useragent'); //тест фикс by мегамозг кабан
+
+// captha plugin
+puppeteer.use(
+    RecaptchaPlugin({
+        provider: {
+            id: '2captcha',
+            token: `2caphtakey`
+        },
+        visualFeedback: true
+    })
+);
+
+puppeteer.use(StealthPlugin()) // стелс хуй пизда картон
+
+// Function
+const r = (l)=>{let r='';const c='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';for(let i=0;i<l;i++){r+=c.charAt(Math.floor(Math.random()*c.length));};return r;};
+const delay = (time)=>{return new Promise(function(resolve){setTimeout(resolve, time);});};
+const randomint = (min, max)=>{return Math.floor(Math.random()*(max-min+1))+min;};
+
+// User info
+const birthday = randomint(1, 28).toString(); 
+const month_of_birth = randomint(1, 12).toString(); 
+const year_of_birth = randomint(1990, 2001).toString();
+const username = UsernameGenerator.generateUsername(); 
+const pwd = r(10);
+
+const proxy = require(`fs`).readFileSync(`./proxies.txt`, 'utf-8').replace(/\r/g, '').split('\n')[Math.floor(Math.random() * require(`fs`).readFileSync(`./proxies.txt`, 'utf-8').replace(/\r/g, '').split('\n').length)]
+
+console.log(`\n Proxy -> ${proxy}`);
+
+// Init browser
+function init_driver() {
+    return new Promise((resolve, reject) => {
+        console.log('Initilizing web driver...\n');
+        puppeteer.launch({
+            ignoreHTTPSErrors: true,
+            headless: false,
+            executablePath: "./WebDriver/chrome.exe",
+            args: [
+                `--proxy-server=socks5://${proxy}`,
+            ]
+        }).then(async browser => {
+            const page = await browser.newPage();
+            //await page.setViewport({width: 411, height: 731});
+            console.log('Webdriver initilized\n');
+            resolve(page)
+        });
+    })
+}
+
+// Main
+(async () => {
+    const usemail = await mailwork.pickmail();
+    const name = usemail.name;
+    const password = usemail.pass;
+
+    function choose(choices) {
+      var index = Math.floor(Math.random() * choices.length);
+      return choices[index];
+    }
+
+    function dotit(email){
+      var username = email.split("@")[0]
+      var dotemail = ""
+      for(i=0;i<username.length-1;i++){
+        dotemail +=username[i]+choose(["", "."])
+      }
+      dotemail +=username[username.length-1]
+      return dotemail
+    }
+
+    const email = dotit(name)+`@gmail.com`
+    const UA = userAgent.getRandom()
+
+    console.log(`\n Mail -> ${email}\n`);
+    
+    // const tor = require('child_process').spawn("./Tor/tor.exe", ["--defaults-torrc", "./Tor/torrc", "-f", "./Tor/bridge.1"]);
+    let page;
+    // tor.stdout.on('data', async function(data) {
+    //     data = data.toString();
+    //     if (data.match(/Bootstrapped 100%/g)) {
+    //         console.log('Tor started...\n');
+    //         page = await init_driver();
+    //         discord_gen_part();
+    //     }
+    // })
+
+    page = await init_driver();
+    discord_gen_part();
+
+    async function discord_gen_part() {
+        await page.goto('https://discord.com/register');
+        await page.setDefaultNavigationTimeout(0);
+
+        console.log("\nПерехожу к регистрации...");
+
+        async function data_entry(){
+            return new Promise(async(resolve, reject) => {
+                await page.type("input[type='email']", email);
+                await page.waitForSelector("input[type='text']");
+                await page.type("input[type='text']", username);
+                await page.waitForSelector("input[type='password']");
+                await page.type("input[type='password']", pwd);
+                resolve("ok")
+            })
+        }
+
+        async function click_date(page, name, min, max) {
+            var i = await page.$('[class*=input' + name + "]");
+            await i.click();
+            var r = Math.floor(Math.random() * (max - min + 1)) + min;
+
+            await page.waitForSelector('[class*=option]');
+            await page.$eval("[class$=option]", function(e, r) {
+                e.parentNode.childNodes[r].click()
+            }, r);
+
+            return r
+        }
+        
+        async function date_of_birth_or_submit() {
+            return new Promise(async(resolve, reject) => {
+
+                await page.waitForSelector("#app-mount > div.app-1q1i1E > div > div > div > form > div > div > div.container-3bTSed.marginTop20-3TxNs6 > div.inputs-14Hc7m > div:nth-child(1) > div > div > div > div > div.css-1fhf191 > div"); 
+                await page.click('#app-mount > div.app-1q1i1E > div > div > div > form > div > div > div.container-3bTSed.marginTop20-3TxNs6 > div.inputs-14Hc7m > div:nth-child(1) > div > div > div > div > div.css-1fhf191 > div');
+
+                await click_date(page, "Year", 17, 24);
+                await click_date(page, "Day", 0, 28);
+                await click_date(page, "Month", 0, 11);
+
+                await delay(200);
+
+                await data_entry();
+
+                await page.waitForSelector('.authBox-hW6HRx > .centeringWrapper-2Rs1dR > .block-egJnc0 > .marginTop20-3TxNs6 > .button-3k0cO7');
+                await page.click('.authBox-hW6HRx > .centeringWrapper-2Rs1dR > .block-egJnc0 > .marginTop20-3TxNs6 > .button-3k0cO7');
+                resolve("ok")
+            })
+        }
+
+        await date_of_birth_or_submit();
+
+        async function captha_solver() {
+            console.log("\n[Captha]", "Решаю капчу");
+
+            await delay(2000);
+
+            await page.solveRecaptchas().then(async govno => {
+                console.log("\n[Captha]", "Капча решана!");
+                await delay(2000);
+                await mailwork.getmessage(name,password).then(x=>{
+                    solve_mail_captha(x);
+                })
+            });
+        }
+
+        await captha_solver();
+    }
+
+    async function get_token() {
+        await page.goto('https://discord.com/channels/@me');
+        await delay(1500);
+        const token_ret = await page.evaluate("(webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()"); //получение токена
+        console.log("\nВыдаю токен: \n", token_ret);
+    }
+
+    async function solve_mail_captha(url) {
+        await page.goto(url);
+        await page.setDefaultNavigationTimeout(60000);
+        await delay(1000);
+        console.log("\n[Captha]", "Решаю капчу на почте");
+        let {
+            captchas,
+            filtered,
+            error
+        } = await page.findRecaptchas();
+        let {
+            solutions,
+            error2
+        } = await page.getRecaptchaSolutions(captchas);
+        let {
+            solved,
+            error3
+        } = await page.enterRecaptchaSolutions(solutions);
+
+        await delay(1000);
+
+        try {
+            if (solved[0].isSolved) {
+                console.log("\n[Captha]", "Решил капчу на почте");
+                //page.eval(`(webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()`);
+                return true;
+            }
+        } catch (e) {
+            get_token();
+        }
+    }
+
+    
+})()
