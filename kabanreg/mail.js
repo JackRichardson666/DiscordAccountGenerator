a52757de9906b25ad1c649d4e4cf6da3cbd95b93
JackRichardson666 <64367044+JackRichardson666@users.noreply.github.com>
2022-02-05 17:24:27 +0300

    Update mail.js
    убрал лишние пробелы

--- kabanreg/mail.js
+++ kabanreg/mail.js
@@ -10,10 +10,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
             mail.getMessage(str, csrf, function(e, b) {
                 if (e) return resolve({success: false,error:e}) ;
-                resolve({
-                    success: true,
-                    message: `https://click.discord.com/` + b.split(`https://click.discord.com/`)[2].split("\"")[0]
-                });
+                resolve({success: true,message: `https://click.discord.com/` + b.split(`https://click.discord.com/`)[2].split("\"")[0]});
             })
         })
     },
@@ -26,42 +23,25 @@ module.exports = {
     },
 
     getmessage: function(mail,password) {
-
         return new Promise((resolve, reject) => {
-            const config = {
-                imap: {
-                    user: mail+"@gmail.com",
-                    password: password,
-                    host: 'imap.gmail.com',
-                    port: 993,
-                    tls: true,
-                    authTimeout: 3000
-                }
-            };
+            const config = {imap: {user: mail+"@gmail.com",password: password,host: 'imap.gmail.com',port: 993,tls: true,authTimeout: 3000}};
 
             imaps.connect(config).then(function (connection) {
                 return connection.openBox('INBOX').then(function () {
                     const searchCriteria = ['UNSEEN'];
-                    const fetchOptions = {
-                        bodies: ['HEADER', 'TEXT', ''],
-                        markSeen: true
-                        };;
+                    const fetchOptions = {bodies: ['HEADER', 'TEXT', ''],markSeen: true};;
                     return connection.search(searchCriteria, fetchOptions).then(function (messages) {
                         messages.forEach(function (item) {
                             const all = _.find(item.parts, { "which": "" })
                             const id = item.attributes.uid;
                             const idHeader = "Imap-Id: "+id+"\r\n";
                             simpleParser(idHeader+all.body, (err, mail) => {
-                                if(mail.subject.search(`Discord`) > -1){
-                                    const krut = mail.html.split(`Подтвердить e-mail`)[0].slice(-2000).match(/http[^ ]+/g)[0].slice(0,-1);
-                                    resolve(krut)
-                                }
+                                if(mail.subject.search(`Discord`) > -1){const krut = mail.html.split(`Подтвердить e-mail`)[0].slice(-2000).match(/http[^ ]+/g)[0].slice(0,-1);resolve(krut)}
                             });
                         });
                     });
                 });
             });
-
         })
     }
 }
